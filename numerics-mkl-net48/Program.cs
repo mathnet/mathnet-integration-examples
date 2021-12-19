58f5cfdf5d452d763b3080dc402895a80fb99708
Christoph Ruegg <git@cdrnet.ch>
2021-12-19 15:52:55 +0100

    .NET Framework 4.8

---
+++ numerics-mkl-net48/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics;
+
+namespace numerics_mkl_net48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Control.Describe());
+            Console.ReadKey();
+        }
+    }
+}
