1e63f33c1b86b7b31b7a888f6f5e3e9b31d1b724
Christoph Ruegg <git@cdrnet.ch>
2021-12-29 19:00:05 +0100

    Update, cover also explicit platform targets for x86 and x64

---
+++ numerics-mkl-net50-x86/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MathNet.Numerics;
+
+namespace numerics_mkl_net50
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Control.Describe());
+        }
+    }
+}
